7353d9e2d2bb3207b5d7e701eab1c84005c5616e
gallais <guillaume.allais@ens-lyon.org>
2014-08-08 12:30:33 +0100

    [ feature ] Building up on yesterday night's idea
    * Using the idea introduced yesterday night, we can define a fully
      general fold and derive two simpler ones (foldRec and cfold').
    
    * We provide three use cases: cfold is used to implement a fully
      general cmap; cfoldRec lets us define a Show instance producing
      *finite strings* and toStream unfolds the cycle using cfold'.

--- haskell/cyclic/CyclicList.hs
+++ haskell/cyclic/CyclicList.hs
@@ -1,44 +1,59 @@
-{-# LANGUAGE GADTs, RankNTypes, FlexibleInstances, ScopedTypeVariables #-}
+{-# LANGUAGE GADTs, RankNTypes, FlexibleInstances,
+    ScopedTypeVariables, KindSignatures #-}
 
 module CyclicList where
 
 import Data.Void
 
-data Finite = Finite
-data Bound  = Bound
 data Closed = Closed
 type List a = CList a Closed
 
 data CList a b where
-  CNil :: CList a b
+  CNil :: CList a Closed
   Cons :: a -> CList a b -> CList a b
   CRec :: a -> (forall b. CList a b -> CList a b) -> CList a Closed
 
--- unfortunately, cmap unfolds the list (cf. CRec case)
--- because the contravariance in CRec makes it impossible
--- to build another CRec.
-cmap :: forall a b. (a -> b) -> List a -> List b
-cmap f xs = goCRec xs
+cfold :: forall a (b :: * -> *).
+         (forall ph. a -> b ph -> b ph) ->
+         b Closed ->
+         (a -> (forall ph. b ph -> b ph) -> b Closed) ->
+         List a ->
+         b Closed
+cfold c n r = goCRec
   where
-    goCRec :: forall ph. CList a ph -> CList b ph
-    goCRec CNil          = CNil
-    goCRec (Cons x xs)   = Cons (f x) $ goCRec xs
-    goCRec xs@(CRec x r) = CRec (f x) $ \ bs -> stopCRec bs (r xs)
+    goCRec :: forall ph. CList a ph -> b ph
+    goCRec CNil          = n
+    goCRec (Cons x xs)   = c x $ goCRec xs
+    goCRec xs@(CRec x p) = r x $ stopCRec (p xs)
 
-    stopCRec :: forall ph. CList b ph -> CList a Closed -> CList b ph
-    stopCRec bs CNil        = CNil
-    stopCRec bs (Cons x xs) = Cons (f x) $ stopCRec bs xs
-    stopCRec bs (CRec _ _)  = bs
+    stopCRec :: CList a Closed -> forall ph. b ph -> b ph
+    stopCRec (Cons x xs) ih = c x $ stopCRec xs ih
+    stopCRec (CRec _ _)  ih = ih
 
-data Subst a b c where
-  Empty :: Subst a b b
-  AnElt :: a -> Subst a () Void
+cmap :: forall a b. (a -> b) -> List a -> List b
+cmap f = cfold (Cons . f) CNil (CRec . f)
+
+newtype Lift a b = Lift { out :: a }
+
+cfoldRec :: forall a b. (a -> b -> b) -> b ->
+            (a -> (b -> b) -> b) -> List a -> b
+cfoldRec c n r xs = out $ cfold c' (Lift n) r' xs
+  where
+    c' :: forall ph. a -> Lift b ph -> Lift b ph
+    c' a    = Lift . c a . out
+    r' :: a -> (forall ph. Lift b ph -> Lift b ph) -> Lift b Closed
+    r' a ih = Lift $ r a (out . ih . Lift)
+
+cfold' :: forall a b. (a -> b -> b) -> b -> List a -> b
+cfold' c n = cfoldRec c n r
+  where
+    r :: a -> (b -> b) -> b
+    r a ih = c a (ih $ r a ih)
 
 ctail :: List a -> List a
 ctail CNil          = CNil
 ctail (Cons _ xs)   = xs
 ctail xs@(CRec x r) = r xs
-
 
 clist1 :: List Int
 clist1 = CRec 1 $ \ xs -> Cons 2 xs
@@ -67,32 +82,25 @@ clist2 = Cons 1 $ CRec 2 $ \ xs -> Cons 3 xs
 -------------------------------------------------------------
 -- Rigtfully rejected: this is not a cyclic definition!
 -------------------------------------------------------------
-{-    Couldn't match type `b' with `Closed'
-      `b' is a rigid type variable bound by
-          a type expected by the context: CList Int b -> CList Int b
-          at Cyclic.hs:28:11
+{-
+    Couldn't match type `(forall b.
+                          CList (List Integer -> CList Integer Closed) b
+                          -> CList (List Integer -> CList Integer Closed) b)
+                         -> CList (List Integer -> CList Integer Closed) Closed'
+                  with `CList Int Closed'
     Expected type: List Int
-      Actual type: CList Int b
-    In the second argument of `cmap', namely `xs'
-    In the second argument of `($)', namely `cmap (+ 1) xs'
+      Actual type: (forall b.
+                    CList (List Integer -> CList Integer Closed) b
+                    -> CList (List Integer -> CList Integer Closed) b)
+                   -> CList (List Integer -> CList Integer Closed) Closed
+    In the expression: CRec $ \ xs -> Cons 1 $ cmap (+ 1) xs
+    In an equation for `acyclic':
+        acyclic = CRec $ \ xs -> Cons 1 $ cmap (+ 1) xs
 -}
 
-data Stop = Stop
-
-stop :: CList a Stop
-stop = CNil
-
-instance Show a => Show (CList a Stop) where
-  show (Cons x xs) = show x ++ " : " ++  show xs
-  show CNil = "X"
-
 instance Show a => Show (List a) where
-  show CNil        = "[]"
-  show (Cons x xs) = show x ++ " : " ++  show xs
-  show (CRec x r)  = "fix X. " ++ show (Cons x $ r stop)
+  show = cfoldRec (\ x -> (++) (show x ++ " : ")) "[]"
+                  (\ x ih -> "rec X. " ++ show x ++ " : " ++ ih "X")
 
 toStream :: List a -> [ a ]
-toStream CNil          = []
-toStream (Cons x xs)   = x : toStream xs
-toStream xs@(CRec x r) = x : toStream (r xs)
-
+toStream = cfold' (:) []
