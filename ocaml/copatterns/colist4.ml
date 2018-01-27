4f7d6e76ec3f813d247408dccd2fa71075c6e52f
Guillaume Allais <guillaume.allais@ens-lyon.org>
2018-01-27 20:58:09 +0100

    [ more ] colist4
    Same type of error as colist1.ml except that we're using a more
    clever encoding inspired by @gasche's comment on reddit

---
+++ ocaml/copatterns/colist4.ml
@@ -0,0 +1,20 @@
+type _ tag =
+  | TNil  : unit tag
+  | TCons : ('a * 'a colist) tag
+and 'a colist =
+  { Tag : 'b tag
+  ; Pld : 'b
+  }
+
+let corec argh : int colist with
+  | ..#Tag -> TNil
+  | ..#Pld -> Cons (1, argh)
+
+(* Same type of error as colist1.ml except that we're using
+   a more clever encoding inspired by @gasche's comment:
+   https://www.reddit.com/r/haskell/comments/7tbw7d/what_i_whish_i_knew_haskell_and_dependent_pairs/dtbdszv/
+
+Error: This expression has type unit tag
+       but an expression was expected of type a0 = $0 tag
+       Type unit is not compatible with type $0
+*)
