92cecd7987f0c13620bd9b06c1a57312b96047e7
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-03-10 14:59:05 +0100

    [ new ] instance for Maybe

--- haskell/defaulting/Default.hs
+++ haskell/defaulting/Default.hs
@@ -45,6 +45,18 @@ data instance Sing Bool Bool b where
 instance Known Bool Bool 'True  where sing = STrue; erase _ = True
 instance Known Bool Bool 'False where sing = SFalse; erase _ = False
 
+data instance Sing (Maybe a) (Maybe b) m where
+  SNothing :: Sing (Maybe a) (Maybe b) 'Nothing
+  SJust    :: Sing a b v -> Sing (Maybe a) (Maybe b) ('Just v)
+
+instance Known (Maybe a) (Maybe b) 'Nothing where
+  sing = SNothing
+  erase _ = Nothing
+
+instance Known a b v => Known (Maybe a) (Maybe b) ('Just v) where
+  sing            = SJust sing
+  erase (SJust v) = Just (erase v)
+
 data instance Sing [a] [b] xs where
   SNil  :: Sing [a] [b] '[]
   SCons :: Sing a b x -> Sing [a] [b] xs -> Sing [a] [b] (x ': xs)
