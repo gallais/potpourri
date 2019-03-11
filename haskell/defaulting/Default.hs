a20fed3c4b7e46c0683eba55788d07f09bbaf776
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-03-11 16:50:40 +0100

    [ cleanup ] no need to take l explicitly!

--- haskell/defaulting/Default.hs
+++ haskell/defaulting/Default.hs
@@ -16,67 +16,65 @@ import Data.Constraint
 import Data.Kind
 import Data.Text
 
-data WithDefault' k l (a :: l) where
-  Default :: WithDefault' k l a
-  Value   :: k -> WithDefault' k l a
+data WithDefault k (a :: l) where
+  Default :: WithDefault k a
+  Value   :: k -> WithDefault k a
 
-type WithDefault (k :: Type) (a :: k) = WithDefault' k k a
-
-setDefault :: k -> WithDefault' k l a -> WithDefault' k l a
+setDefault :: k -> WithDefault k a -> WithDefault k a
 setDefault k v = case v of
   Default -> Value k
   _ -> v
 
-collapseDefault :: forall k l a. Known k l a => WithDefault' k l a -> k
+collapseDefault :: forall k l (a :: l). Known k a => WithDefault k a -> k
 collapseDefault x = case x of
-  Default -> erase (sing :: Sing k l a)
+  Default -> erase (sing :: Sing k a)
   Value v -> v
 
-data family Sing k l (a :: l)
+data family Sing k (a :: l)
 
-class Known k l (a :: l) | a -> l where
-  sing  :: Sing k l a
-  erase :: Sing k l a -> k
+class Known k (a :: l) where
+  sing  :: Sing k a
+  erase :: Sing k a -> k
 
-data instance Sing Bool Bool b where
-  STrue  :: Sing Bool Bool 'True
-  SFalse :: Sing Bool Bool 'False
+data instance Sing Bool b where
+  STrue  :: Sing Bool 'True
+  SFalse :: Sing Bool 'False
 
-instance Known Bool Bool 'True  where sing = STrue; erase _ = True
-instance Known Bool Bool 'False where sing = SFalse; erase _ = False
+instance Known Bool 'True  where sing = STrue; erase _ = True
+instance Known Bool 'False where sing = SFalse; erase _ = False
 
-data instance Sing (Maybe a) (Maybe b) m where
-  SNothing :: Sing (Maybe a) (Maybe b) 'Nothing
-  SJust    :: Sing a b v -> Sing (Maybe a) (Maybe b) ('Just v)
+data instance Sing (Maybe a) (m :: Maybe b) where
+  SNothing :: Sing (Maybe a) 'Nothing
+  SJust    :: Sing a v -> Sing (Maybe a) ('Just v)
 
-instance Known (Maybe a) (Maybe b) 'Nothing where
+instance Known (Maybe a) ('Nothing :: Maybe b) where
   sing = SNothing
   erase _ = Nothing
 
-instance Known a b v => Known (Maybe a) (Maybe b) ('Just v) where
+instance Known a (v :: b) => Known (Maybe a) ('Just v :: Maybe b) where
   sing            = SJust sing
   erase (SJust v) = Just (erase v)
 
-data instance Sing [a] [b] xs where
-  SNil  :: Sing [a] [b] '[]
-  SCons :: Sing a b x -> Sing [a] [b] xs -> Sing [a] [b] (x ': xs)
+data instance Sing [a] (xs :: [b]) where
+  SNil  :: Sing [a] ('[] :: [b])
+  SCons :: Sing a (x :: b) -> Sing [a] xs -> Sing [a] (x ': xs)
 
-instance Known [a] [b] '[] where sing = SNil; erase _ = []
-instance (Known a b x, Known [a] [b] xs) => Known [a] [b] (x ': xs) where
+instance Known [a] ('[] :: [b]) where sing = SNil; erase _ = []
+instance (Known a (x :: b), Known [a] xs) => Known [a] (x ': xs) where
   sing               = SCons sing sing
   erase (SCons x xs) = erase x : erase xs
 
-data instance Sing Text Symbol s where
-  SText :: KnownSymbol s => Sing Text Symbol s
+data instance Sing Text (s :: Symbol) where
+  SText :: KnownSymbol s => Sing Text s
 
-instance KnownSymbol s => Known Text Symbol s where
+instance KnownSymbol s => Known Text s where
   sing  = SText
   erase = pack . symbolVal
 
-data instance Sing (a, b) (c, d) '(l , r) where
-  SPair :: Sing a c l -> Sing b d r -> Sing (a, b) (c, d) '(l , r)
+data instance Sing (a, b) ('(l , r) :: (c, d)) where
+  SPair :: Sing a (l :: c) -> Sing b (r :: d) -> Sing (a, b) '(l , r)
 
-instance (Known a c l, Known b d r) => Known (a, b) (c, d) '(l , r) where
+instance (Known a (l :: c), Known b (r :: d)) => Known (a, b) '(l , r) where
   sing              = SPair sing sing
   erase (SPair l r) = (erase l, erase r)
 
@@ -89,10 +87,9 @@ bool = collapseDefault <$>
 
 test :: [[(Bool, Text)]]
 test = collapseDefault <$>
-     [ Default :: WithDefault' [(Bool, Text)] [(Bool, Symbol)] '[ '( 'True , "Hello"), '( 'False , "World") ]
+     [ Default :: WithDefault [(Bool, Text)] '[ '( 'True , "Hello"), '( 'False , "World") ]
      , Value [(True, "")]
      , Value []
      , Default
      ]
-
 
