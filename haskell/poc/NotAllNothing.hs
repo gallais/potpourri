52e9cd4737cb8f447110a2480eba551c0003df83
Guillaume Allais <guillaume.allais@ens-lyon.org>
2022-07-05 09:58:18 +0100

    [ poc ] not all Nothing

---
+++ haskell/poc/NotAllNothing.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Data.Kind
+
+data IMaybe (b :: Bool) (a :: Type) where
+  IJust :: a -> IMaybe True a
+  INothing :: IMaybe False a
+
+data So (b :: Bool) :: Type where
+  So :: So True
+
+type family (||) (b :: Bool) (c :: Bool) :: Bool where
+  True || c = True
+  False || c = c
+
+data A = A
+data B = B
+data C = C
+
+data MyType = forall a b c. (a || b || c) ~ True => MyType
+  { myTypeA :: IMaybe a A
+  , myTypeB :: IMaybe b B
+  , myTypeC :: IMaybe c C
+  }
+
+myValC :: MyType
+myValC = MyType INothing INothing (IJust C)
+
+myValAC :: MyType
+myValAC = MyType (IJust A) INothing (IJust C)
+
+fail :: MyType
+fail = MyType INothing INothing INothing
