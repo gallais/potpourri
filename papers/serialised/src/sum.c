58e924133adb61e9545a994aca53e989e9cd11d1
Guillaume Allais <guillaume.allais@ens-lyon.org>
2023-06-09 11:36:01 +0100

    [ fix ] sum.c to use bytes, return void

--- papers/serialised/src/sum.c
+++ papers/serialised/src/sum.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-int sumAt (int buf[], int *ptr, int *res, char *str) {
+void sumAt (uint8_t buf[], int *ptr, int *res, char *str) {
   // read the tag, move the pointer past it
-  int tag = buf[*ptr]; (*ptr)++;
+  uint8_t tag = buf[*ptr]; (*ptr)++;
   switch (tag) {
     case 0: // a leaf
       sprintf(str, "%sleaf", str);
-      return 0;
+      return;
     case 1: // a node
       sprintf(str, "%s(node ", str);
-      sumAt(buf, ptr, res, str) < 0; // sum the left subtree
+      sumAt(buf, ptr, res, str); // sum the left subtree
       // read the stored value, move the pointer past it
-      int val  = buf[*ptr]; (*ptr)++;
-      *res += val; // add the value to the accumulator
+      uint8_t val = buf[*ptr]; (*ptr)++;
+      *res += (int) val; // add the value to the accumulator
       sprintf(str, "%s %d ", str, val);
-      sumAt(buf, ptr, res, str) < 0; // sum the right subtree
+      sumAt(buf, ptr, res, str); // sum the right subtree
       sprintf(str, "%s)", str);
-      return 0;
+      return;
     default:
-      printf("Invalid format\n");
+      printf("Invalid format: %d\n", tag);
       exit(-1);
 }}
 
 int main () {
-  int buf[] = {1, 0, 10, 1, 1, 0, 100, 1, 0, 1, 2, 20, 0};
+  uint8_t buf[] = {1, 1, 0, 4, 1, 0, 0, 0, 10, 1, 1, 0, 100, 1, 0, 1, 0, 20, 0};
   int ptr = 0;
   int sum = 0;
   char str[10000];
