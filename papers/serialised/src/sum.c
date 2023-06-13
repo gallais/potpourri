faad289e44841f871a2be843204dc8e7f2488034
Guillaume Allais <guillaume.allais@ens-lyon.org>
2023-06-13 13:29:14 +0100

    [ doc ] more tweaks
    including bringing the C program closer to the Idris 2 function.

--- papers/serialised/src/sum.c
+++ papers/serialised/src/sum.c
@@ -2,23 +2,22 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-void sumAt (uint8_t buf[], int *ptr, int *res, char *str) {
+int sumAt (uint8_t buf[], int *ptr, char *str) {
   // read the tag, move the pointer past it
   uint8_t tag = buf[*ptr]; (*ptr)++;
   switch (tag) {
     case 0: // a leaf
       sprintf(str, "%sleaf", str);
-      return;
+      return 0;
     case 1: // a node
       sprintf(str, "%s(node ", str);
-      sumAt(buf, ptr, res, str); // sum the left subtree
+      int m = sumAt(buf, ptr, str); // sum the left subtree
       // read the stored value, move the pointer past it
       uint8_t val = buf[*ptr]; (*ptr)++;
-      *res += (int) val; // add the value to the accumulator
       sprintf(str, "%s %d ", str, val);
-      sumAt(buf, ptr, res, str); // sum the right subtree
+      int n = sumAt(buf, ptr, str); // sum the right subtree
       sprintf(str, "%s)", str);
-      return;
+      return (m + (int) val + n);
     default:
       printf("Invalid format: %d\n", tag);
       exit(-1);
@@ -28,9 +27,8 @@ int main () {
   // uint8_t buf[] = {1, 1, 0, 4, 1, 0, 0, 0, 10, 1, 1, 0, 100, 1, 0, 1, 0, 20, 0};
   uint8_t buf[] = {01, 0x01, 0x01, 0x00, 0x01, 0x00, 0x05, 0x00, 0x0a, 0x01, 0x00, 0x14, 0x00};
   int ptr = 0;
-  int sum = 0;
   char str[10000];
-  sumAt(buf, &ptr, &sum, str);
+  int sum = sumAt(buf, &ptr, str);
   printf("%s\nSum: %d\n", str, sum);
   return 0;
 }
