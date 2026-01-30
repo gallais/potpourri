ccb0657f2d7c15be2863f9467285eca6a674a8d0
Guillaume Allais <guillaume.allais@ens-lyon.org>
2026-01-30 14:48:14 +0000

    [ new ] adding join points

---
+++ haskell/concurrency/Conc4.hs
@@ -0,0 +1,163 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Conc4 where
+
+import Data.Functor (void)
+import Data.Kind (Type)
+
+------------------------------------------------------------------------
+-- Continuation monad
+
+newtype Cont r a = MkCont { runCont :: (a -> r) -> r }
+
+instance Functor (Cont r) where
+  fmap f (MkCont c) = MkCont (c . (. f))
+
+instance Applicative (Cont r) where
+  pure a = MkCont ($ a)
+  MkCont mf <*> MkCont ma = MkCont $ \ k ->
+    mf $ \ f ->
+    ma $ \ a ->
+    k (f a)
+
+instance Monad (Cont r) where
+  MkCont ma >>= f = MkCont $ \ k ->
+    ma $ \ a ->
+    runCont (f a) k
+
+execCont :: Cont a a -> a
+execCont (MkCont c) = c id
+
+------------------------------------------------------------------------
+-- Concurrency monad
+
+data Action m a where
+  Pure :: a -> Action m a
+  Atom :: m (Action m a) -> Action m a
+  Fork :: Action m ()
+       -> Action m a
+       -> Action m a
+  Join :: Action m b
+       -> Action m c
+       -> (b -> c -> Action m a)
+       -> Action m a
+
+type Conc m a = forall r. Cont (Action m r) a
+
+action :: Conc m a -> Action m a
+action (MkCont c) = c Pure
+
+atom :: Functor m => m a -> Conc m a
+atom m = MkCont $ \ k -> Atom (k <$> m)
+
+fork :: Conc m () -> Conc m ()
+fork c = MkCont (\ k -> Fork (action c) (k ()))
+
+conc :: Conc m a -> Conc m b -> Conc m (a, b)
+conc c1 c2 = MkCont $ \ k ->
+  Join (action c1) (action c2) $ \ a b ->
+  k (a, b)
+
+type family TreeVal (t :: Tree a) :: a where
+  TreeVal (Leaf x) = x
+  TreeVal (Node x _ _) = x
+
+data Tree a = Leaf a | Node a (Tree a) (Tree a)
+
+data Layer a
+  = TreeL a () (Tree a)
+  | TreeR a (Tree a) ()
+
+data TreeAct m a where
+  AValue :: a -> TreeAct m a
+  ALeaf  :: Action m a -> TreeAct m a
+  ANode  :: (a -> b -> Action m c)
+         -> TreeAct m a
+         -> TreeAct m b
+         -> TreeAct m c
+
+data Stack m :: Type -> Type -> Type where
+  SNil   :: Stack m a a
+  SConsL :: (a -> b -> Action m c)
+         -> Stack m c ret
+         -> TreeAct m b
+         -> Stack m a ret
+  SConsR :: (a -> b -> Action m c)
+         -> TreeAct m a
+         -> Stack m c ret
+         -> Stack m b ret
+
+unwind
+  :: Monad m
+  => a
+  -> Stack m a ret
+  -> m ret
+unwind v SNil = pure v
+unwind v (SConsL f stk t) = case t of
+  AValue b -> next (ALeaf $ f v b) stk
+  _ -> robin t (SConsR f (AValue v) stk)
+unwind v (SConsR f t stk) = case t of
+  AValue a -> next (ALeaf $ f a v) stk
+  _ -> next (ANode f t (AValue v)) stk
+
+next
+  :: Monad m
+  => TreeAct m a
+  -> Stack m a ret
+  -> m ret
+next t SNil = robin t SNil
+next t (SConsL f stk t') = robin t' (SConsR f t stk)
+next t (SConsR f t' stk) = next (ANode f t' t) stk
+
+robin
+  :: Monad m
+  => TreeAct m a
+  -> Stack m a ret
+  -> m ret
+robin (AValue v) stk = unwind v stk
+robin (ALeaf a) stk = case a of
+  Pure v -> unwind v stk
+  Atom c -> do
+    a <- c
+    next (ALeaf a) stk
+  Fork a1 a2 -> next (ANode (const Pure) (ALeaf a1) (ALeaf a2)) stk
+  Join a1 a2 j -> next (ANode j (ALeaf a1) (ALeaf a2)) stk
+robin (ANode f a1 a2) stk = robin a1 (SConsL f stk a2)
+
+
+run :: Monad m => Conc m a -> m a
+run c = robin (ALeaf (action c)) SNil
+
+loop :: Int -> String -> Conc IO Int
+loop i s
+  | i <= 0 = pure (length s)
+  | otherwise = do atom (putStrLn s); loop (i - 1) s
+
+example :: Conc IO Int
+example = do
+  atom $ putStrLn "start!"
+  fork (void $ loop 5 "fish")
+  i <- do fork (void $ loop 5 "cat"); loop 5 "catfish"
+  j <- loop 5 "finalising"
+  atom $ putStrLn "end!"
+  atom $ pure (i + j)
+
+fibonacci :: Int -> Int -> Conc IO Int
+fibonacci i n | n <= 1 = do
+  atom $ putStrLn $ unwords
+    [ "Thread", show i, "has reached the base case: fib", show n, "=", show n ]
+  pure n
+fibonacci i n = do
+  let i1 = (2*i)
+  let i2 = i1+1
+  atom $ putStrLn $ unwords
+    [ "Thread", show i, "is spawning threads"
+    , show i1, "and", show i2
+    , "concurrently"]
+  (fn1, fn2) <- conc (fibonacci i1 (n-1)) (fibonacci i2 (n-2))
+  let fn = fn1 + fn2
+  atom $ putStrLn $ unwords
+    [ "Thread", show i, "has computed fib", show n, "=", show fn ]
+  pure (fn1 + fn2)
